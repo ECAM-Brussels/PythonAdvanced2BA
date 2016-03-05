a1850ec159cfe87f7375463b9f2c6d12992426b5
Sébastien Combéfis <seb478@gmail.com>
2016-03-06 00:29:40 +0100

    Add sched module use example

---
+++ CodeExamples/concurrency/clock.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# clock.py
+# author: Sébastien Combéfis
+# version: March 6, 2016
+
+import sched
+from datetime import datetime
+import sys
+import time
+
+def printhour():
+    print('\r{0:%H:%M:%S}'.format(datetime.now()), end='')
+    sys.stdout.flush()
+    scheduler.enter(1, 1, printhour)
+
+scheduler = sched.scheduler(time.time, time.sleep)
+scheduler.enter(1, 1, printhour)
+scheduler.run()
