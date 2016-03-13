8d33416ad5bc3fc75ad498fb443b546ec9dc5a29
Sébastien Combéfis <seb478@gmail.com>
2016-03-13 14:21:55 +0100

    Update simpleai library example
    - Added greedy search example
    - Added time comparison between BFS and greedy

--- CodeExamples/ai/findaword.py
+++ CodeExamples/ai/findaword.py
@@ -3,15 +3,18 @@
 # author: Sébastien Combéfis
 # version: March 12, 2016
 
+import time
+
 from simpleai.search import SearchProblem
 from simpleai.search import breadth_first
+from simpleai.search import greedy
 
 GOAL = 'HELLO'
 
 class HelloProblem(SearchProblem):
     def actions(self, state):
         if len(state) < len(GOAL):
-            return list(' DEHLORW')
+            return list(' ABCDEFGHIJKLMNOPQRSTUVWXYZ')
         return []
 
     def result(self, state, action):
@@ -19,8 +22,24 @@ class HelloProblem(SearchProblem):
 
     def is_goal(self, state):
         return state == GOAL
+    
+    def heuristic(self, state):
+        wrong = sum([1 if state[i] != GOAL[i] else 0 for i in range(len(state))])
+        missing = len(GOAL) - len(state)
+        return wrong + missing
 
+# Using Breadth-First Search
+t1 = time.time()
 problem = HelloProblem(initial_state='')
 result = breadth_first(problem)
+print('Time: {:.5f}s'.format(time.time() - t1))
+print(result.state)
+print(result.path())
+
+# Using Greedy Search
+t1 = time.time()
+problem = HelloProblem(initial_state='')
+result = greedy(problem)
+print('Time: {:.5f}s'.format(time.time() - t1))
 print(result.state)
 print(result.path())
