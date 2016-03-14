0f4e2b1e5eafa695c2427d9e33e1ad7c5b9916d0
Sébastien Combéfis <seb478@gmail.com>
2016-03-14 15:01:25 +0100

    Update easyAI code example

--- CodeExamples/ai/simplenim.py
+++ CodeExamples/ai/simplenim.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # simplenim.py
-# author: Sébastien Combéfis
+# author: SÃ©bastien CombÃ©fis
 # version: March 14, 2016
 
 from easyAI import TwoPlayersGame, Human_Player, AI_Player, Negamax
@@ -8,23 +8,23 @@ from easyAI import TwoPlayersGame, Human_Player, AI_Player, Negamax
 class SimpleNim(TwoPlayersGame):
     def __init__(self, players):
         self.players = players
-        self.pile = 5
         self.nplayer = 1
+        self.__sticks = 5
     
     def possible_moves(self):
-        return ['1', '2', '3']
+        return [str(v) for v in (1, 2, 3) if v <= self.__sticks]
     
     def make_move(self,move):
-        self.pile -= int(move)
+        self.__sticks -= int(move)
     
     def win(self):
-        return self.pile <= 0
+        return self.__sticks <= 0
     
     def is_over(self):
         return self.win()
     
     def show(self):
-        print('{} sticks left in the pile'.format(self.pile))
+        print('{} sticks left in the pile'.format(self.__sticks))
     
     def scoring(self):
         return 1 if self.win() else 0
