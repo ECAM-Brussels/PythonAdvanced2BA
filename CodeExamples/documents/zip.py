5e93bbf8af24abdc39b46f40fa7d594f009118d2
Sébastien Combéfis <seb478@gmail.com>
2016-03-20 13:26:11 +0100

    Add an example manipulating a ZIP archive

---
+++ CodeExamples/documents/zip.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# zip.py
+# author: Sébastien Combéfis
+# version: March 20, 2016
+
+import os.path
+import zipfile
+
+# Creates a ZIP archive with one file inside
+with zipfile.ZipFile('data.zip', 'w') as file:
+    file.write('data.txt')
+
+# Opens the ZIP archive to get information
+file = zipfile.ZipFile('data.zip')
+file.printdir()
+print(file.getinfo('data.txt'))
+
+# Extracts the content of the ZIP archive
+file.extractall(os.path.expanduser("~"))
