cda12b672cd3d3f1202f88084fe5c806abb4a05b
Sébastien Combéfis <seb478@gmail.com>
2016-04-05 23:54:23 +0200

    Update matplotlib example

--- CodeExamples/math/plot.py
+++ CodeExamples/math/plot.py
@@ -7,7 +7,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(-2*np.pi, 2*np.pi, 256, endpoint=True)
-y = np.cos(x)
+c = np.cos(x)
+s = np.sin(x)
 
-plt.plot(x, y)
+plt.plot(x, c, linewidth=2.5, label='cos(x)')
+plt.plot(x, s, color='red', label='sin(x)')
+
+plt.ylim(-2, 2)
+plt.xticks(
+    [-2*np.pi, -np.pi, 0, np.pi, 2*np.pi],
+    [r'$-2\pi$', r'$\pi$', '0', r'$\pi$', r'$2\pi$']
+)
+plt.legend(loc='upper right')
 plt.show()
