efd82b0a4f083db01e05340af465287af9ca624d
Mathieu David <mathieudavid@mathieudavid.org>
2016-04-14 13:55:08 +0200

    Allows enabling verbose mode with a short flag -v in addition to the long form --verbose

--- AIproject/kingandassassins.py
+++ AIproject/kingandassassins.py
@@ -60,14 +60,14 @@ class KingAndAssassinsState(game.GameState):
     '''Class representing a state for the King & Assassins game.'''
     def __init__(self, initialstate=KA_INITIAL_STATE):
         super().__init__(initialstate)
-    
+
     def update(self, moves, player):
         pass
-    
+
     def winner(self):
         state = self._state['state']
         pass
-    
+
     def prettyprint(self):
         pass
 
@@ -76,7 +76,7 @@ class KingAndAssassinsServer(game.GameServer):
     '''Class representing a server for the King & Assassins game'''
     def __init__(self, verbose=False):
         super().__init__('King & Assassins', 2, KingAndAssassinsState(), verbose=verbose)
-    
+
     def applymove(self, move):
         pass
 
@@ -86,10 +86,10 @@ class KingAndAssassinsClient(game.GameClient):
     def __init__(self, name, server, verbose=False):
         super().__init__(server, KingAndAssassinsState, verbose=verbose)
         self.__name = name
-    
+
     def _handle(self, message):
         pass
-    
+
     def _nextmove(self, state):
         pass
 
@@ -102,13 +102,13 @@ if __name__ == '__main__':
     server_parser = subparsers.add_parser('server', help='launch a server')
     server_parser.add_argument('--host', help='hostname (default: localhost)', default='localhost')
     server_parser.add_argument('--port', help='port to listen on (default: 5000)', default=5000)
-    server_parser.add_argument('--verbose', action='store_true')
+    server_parser.add_argument('-v', '--verbose', action='store_true')
     # Create the parser for the 'client' subcommand
     client_parser = subparsers.add_parser('client', help='launch a client')
     client_parser.add_argument('name', help='name of the player')
     client_parser.add_argument('--host', help='hostname of the server (default: localhost)', default='localhost')
     client_parser.add_argument('--port', help='port of the server (default: 5000)', default=5000)
-    client_parser.add_argument('--verbose', action='store_true')
+    client_parser.add_argument('-v', '--verbose', action='store_true')
     # Parse the arguments of sys.args
     args = parser.parse_args()
     if args.component == 'server':
